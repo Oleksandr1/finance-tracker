212f22c829f2089cfdf3cbd34ef85a7b4e6d208c
Oleksandr Shcherbyna <sanya071186@gmail.com>
2025-03-02 11:53:56 +0200

    add runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,9 @@ export default defineNuxtConfig({
   supabase: {
     redirect: true,
   },
-  // eslint: {
-  //   config: {
-  //     stylistic: {
-  //       indent: 'tab',
-  //       semi: true,
-  //       // ...
-  //     }
-  //   }
-  // }
+runtimeConfig:{
+    public:{
+      baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
+    }
+}
 });
--- pages/login.vue
+++ pages/login.vue
@@ -41,6 +41,7 @@ const pending = ref(false);
 const { toastError } = useAppToast();
 const supabase = useSupabaseClient();
 
+const redirectUrl = useRuntimeConfig().public?.baseUrl
 useRedirectIfAuthenticated();
 
 const handleLogin = async () => {
@@ -49,7 +50,7 @@ const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithOtp({
       email: email.value,
       options: {
-        emailRedirectTo: "http://localhost:3000/confirm",
+        emailRedirectTo: `${redirectUrl}/confirm`,
       },
     });
 
